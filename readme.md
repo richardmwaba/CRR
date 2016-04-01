538fd5f4d217a949a7c94219d3092efcb36f0bf1
in3rtia <rm1a14@cs.unza.zm>
2016-04-01 02:18:49 -0700

    Added Views

---
+++ readme.md
@@ -0,0 +1,5 @@
+   The Contracts Renewal System, hereinafter called the system, was developed to make the operations of the contracts office much easier. It was noticed that employees often delay to renew their contracts or even take long to start the renewal process. With this system we expect that employees will  be reminded on time to start the renewal process and will make the works of the contracts officer easier. 
+
+   It's important to note that this system is not a human resource system rather a supplement to it.
+
+   The Contract renewal system was developed by two computer science Lecturers and four students namely Mr.Phiri,Mr. Sinkala, Manaseh Mwansa, Martin Chileshe, Richard Mwaba and David Bwembya respectively.
---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,96 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Register</div>
+                <div class="panel-body">
+                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/register') }}">
+                        {!! csrf_field() !!}
+
+                        <div class="form-group{{ $errors->has('first-name') ? ' has-error' : '' }}">
+                            <label class="col-md-4 control-label">Firs name</label>
+
+                            <div class="col-md-6">
+                                <input type="text" class="form-control" name="first-name" value="{{ old('first-name') }}">
+
+                                @if ($errors->has('first-name'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('first-name') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('last-name') ? ' has-error' : '' }}">
+                            <label class="col-md-4 control-label">Last name</label>
+
+                            <div class="col-md-6">
+                                <input type="text" class="form-control" name="name" value="{{ old('last-name') }}">
+
+                                @if ($errors->has('last-name'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('last-name') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('man-number') ? ' has-error' : '' }}">
+                            <label class="col-md-4 control-label">Man number</label>
+
+                            <div class="col-md-6">
+                                <input type="text" class="form-control" name="name" value="{{ old('man-number') }}">
+
+                                @if ($errors->has('man-number'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('man-number') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                            <label class="col-md-4 control-label">E-Mail Address</label>
+
+                            <div class="col-md-6">
+                                <input type="email" class="form-control" name="email" value="{{ old('email') }}">
+
+                                @if ($errors->has('email'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                            <label class="col-md-4 control-label">Password</label>
+
+                            <div class="col-md-6">
+                                <input type="password" class="form-control" name="password">
+
+                                @if ($errors->has('password'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                <button type="submit" class="btn btn-primary">
+                                    <i class="fa fa-btn fa-user"></i>Register
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
