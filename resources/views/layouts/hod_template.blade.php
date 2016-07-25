dfcbc94c7acb2c13d8e9f3c4446c1d061e21c090
app_c <mc1a14@cs.unza.zm>
2016-07-25 15:49:55 +0600

    few fixes

--- resources/views/layouts/hod_template.blade.php
+++ resources/views/layouts/hod_template.blade.php
@@ -275,6 +275,77 @@
 <!-- IMage cropping-->
 <script src="{{URL::asset('../dist/js/cropping/cropper.min.js')}}"></script>
 <script src="{{URL::asset('../dist/js/cropping/main.js')}}"></script>
+
+<script>
+
+    $(window).load(function () {
+
+        var date = new Date();
+        var d = date.getDate();
+        var m = date.getMonth();
+        var y = date.getFullYear();
+        var started;
+        var categoryClass;
+
+        var calendar = $('#calendar').fullCalendar({
+            header: {
+                left: 'prev,next today',
+                center: 'title',
+                right: 'month,agendaWeek,agendaDay'
+            },
+            selectable: true,
+            selectHelper: true,
+            select: function (start, end, allDay) {
+                $('#fc_create').click();
+
+                started = start;
+                ended = end
+
+                $(".antosubmit").on("click", function () {
+                    var title = $("#title").val();
+                    if (end) {
+                        ended = end
+                    }
+                    categoryClass = $("#event_type").val();
+
+                    if (title) {
+                        calendar.fullCalendar('renderEvent', {
+                                    title: title,
+                                    start: started,
+                                    end: end,
+                                    allDay: allDay
+                                },
+                                true // make the event "stick"
+                        );
+                    }
+                    $('#title').val('');
+                    calendar.fullCalendar('unselect');
+
+                    $('.antoclose').click();
+
+                    return false;
+                });
+            },
+            eventClick: function (calEvent, jsEvent, view) {
+                //alert(calEvent.title, jsEvent, view);
+
+                $('#fc_edit').click();
+                $('#title2').val(calEvent.title);
+                categoryClass = $("#event_type").val();
+
+                $(".antosubmit2").on("click", function () {
+                    calEvent.title = $("#title2").val();
+
+                    calendar.fullCalendar('updateEvent', calEvent);
+                    $('.antoclose2').click();
+                });
+                calendar.fullCalendar('unselect');
+            },
+            editable: true,
+            events: []
+        });
+    });
+</script>
 @show
 
 
